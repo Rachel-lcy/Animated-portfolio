8bad3d66f3e9219c37b9a38c257ff56c4b819dfb
Rachel <hurui.lcy@gmail.com>
2024-12-12 21:24:11 -0700

    update

---
+++ js/color-mode.js
@@ -0,0 +1,47 @@
+//Define DOM elements
+
+const toggleButton = document.querySelector("#toggle-button");
+// console.log(toggleButton);
+const root = document.querySelector(":root");
+const storageKey =  "color-mode";
+const defaultMode = "light";
+
+//Load the user's preferred color mode from local storage.
+
+function loadColorMode(){
+  //some-code functionality here to run.
+  const colorMode = localStorage.getItem(storageKey);
+  root.classList.add(colorMode || defaultMode);
+  updateToggleButton();
+}
+loadColorMode();
+
+//Toggle the color mode
+
+toggleButton.addEventListener("click",()=>{
+  console.log("test");
+  //some-code functionality here to run.
+  saveColorMode();
+});
+
+//Save the user's preferred color to local storage
+
+function saveColorMode(){
+  //check if the root element has a class of "dark-mode", If yes, then the current mode is switched to light, and vice versa.
+  const currentMode = root.classList.contains("dark-mode")
+  ? "light-mode"
+  : "dark-mode";
+  root.classList.remove("light-mode","dark-mode");
+  root.classList.add(currentMode);
+  localStorage.setItem(storageKey,currentMode);
+  updateToggleButton();
+}
+function updateToggleButton(){
+  if (root.classList.contains("dark-mode")){
+    //code
+    toggleButton.style.backgroundImage="var(--moon)"
+  }else{
+    //code
+     toggleButton.style.backgroundImage="var(--sun)"
+  }
+}
