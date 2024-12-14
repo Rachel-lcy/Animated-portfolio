d97bac6052c2549d30e230b9ed08261abc6c3be7
Rachel <hurui.lcy@gmail.com>
2024-12-13 22:29:54 -0700

    updates css and js

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,28 @@
-/* Hover animation*/
+//Define DOM elements
 
-@keyframes hover{
- 0% {
-    transform: translateY(0);
-   /*some code*/
-    }
- 50% {
-  transform: translateY(-5px);
-  /*some other code*/
-    }
-100% {
-  transform: translateY(0);
-  /*some additional code*/
-    }
-}
+const heroImage = document.querySelector("#hero__animation__image");
+
+const tl = document.querySelector("#grid__tl");
+const tr = document.querySelector("#grid__tr");
+const bl = document.querySelector("#grid__bl");
+const br = document.querySelector("#grid__br");
+
+const tlBtn = document.querySelector("#grid__tl__btn");
+const trBtn = document.querySelector("#grid__tr__btn");
+const blBtn = document.querySelector("#grid__bl__btn");
+const brBtn = document.querySelector("#grid__br__btn");
+
+const tlContent = document.querySelector("#grid__tl__Content");
+const trContent = document.querySelector("#grid__tr__Content");
+const blContent = document.querySelector("#grid__bl__Content");
+const brContent = document.querySelector("#grid__br__Content");
+
+const projectOne = document.querySelector(".p-1");
+const projectTwo = document.querySelector(".p-2");
+const projectThree = document.querySelector(".p-3");
+
+//Define colors and positions
+const bgColor = "var(--bg)";
+const bgColorAlt = "var(--bg-alt)";
+const textColor = "var(--text)";
+const textColorAlt = "var(--text-alt)";
