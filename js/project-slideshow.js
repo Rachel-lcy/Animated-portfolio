d33a5f23f5ee594d76df501145ad5be687fec223
Rachel <hurui.lcy@gmail.com>
2024-12-20 22:07:07 -0700

    projects update2

--- js/project-slideshow.js
+++ js/project-slideshow.js
@@ -1,1 +1,11 @@
 let project = document.getElementsByClassName("project");
+let slideIndex  = 1;
+
+function showProject(){
+// Go to first project, after clicking forward from last button
+}
+
+function navigateProject(){
+  //Change the slideIndex based on back or forward arrow
+
+}
